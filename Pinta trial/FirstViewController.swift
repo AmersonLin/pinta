78937cacb6a8b0a0109e4d1e256038840184cf50
KT Khoo <khoodose@gmail.com>
2015-11-07 20:39:00 +0800

    Initial Commit

---
+++ Pinta trial/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pinta trial
+//
+//  Created by KT Khoo on 7/11/15.
+//  Copyright © 2015 KT Khoo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
