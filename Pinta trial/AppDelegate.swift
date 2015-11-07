bbb743c59e7d47424fbc110e7237b19686dfe3d3
KT Khoo <khoodose@gmail.com>
2015-11-08 00:17:39 +0800

    with parse

--- Pinta trial/AppDelegate.swift
+++ Pinta trial/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Parse
+import Bolts
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.enableLocalDatastore()
+        
+        Parse.setApplicationId("PeyIv22Dbqxb52kNtjEqthDLZmNNy7RykGz0UUaC",
+            clientKey: "U8l0cZnRhQOBqC1HAY6VxqYPsrh4w4EePVLC0JRF")
+        
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         
         let color = UIColor(red: 247.0/255.0, green: 150.0/255.0, blue: 30.0/255.0, alpha: 1.0)
         
