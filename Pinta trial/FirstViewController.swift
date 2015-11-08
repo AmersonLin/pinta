6720212860f6724696158d9fe73026deb8a25f7a
KT Khoo <khoodose@gmail.com>
2015-11-08 10:16:18 +0800

    morn

--- Pinta trial/FirstViewController.swift
+++ Pinta trial/FirstViewController.swift
@@ -11,6 +11,66 @@ import Parse
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var usernameTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var messageLabel: UILabel!
+    
+    
+    
+    @IBAction func loginVerifyButton(sender: AnyObject) {
+        
+        func userSignUp() {
+            
+        }
+        
+        var usrEntered = usernameTextField.text
+        
+        var pwdEntered = passwordTextField.text
+        
+        var emlEntered = emailTextField.text
+        
+        if usrEntered != "" && pwdEntered != "" && emlEntered != "" {
+            
+            userSignUp()
+            
+        } else {
+            
+            self.messageLabel.text = "All Fields Required"
+            
+        }
+        
+        var user = PFUser()
+        
+        user.username = usrEntered
+        
+        user.password = pwdEntered
+        
+        user.email = emlEntered
+        
+        user.signUpInBackgroundWithBlock {
+            
+            (succeeded: Bool!, error: NSError!) -> Void in
+            
+            if error == nil {
+                
+                // Hooray! Let them use the app now.
+                
+                self.messageLabel.text = "User Signed Up";
+                
+            } else {
+                
+                self.messageLabel.text = "All Fields Required"
+                
+            }
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
